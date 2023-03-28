9e5528cb1c722714b263576d27b101198f0571a5
Matheus Fracalossi <mc.fracalossi@amaggi.com.br>
2023-03-27 22:07:13 -0300

    feat(Login): implemente tela de login

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,5 +1,83 @@
-import { Text } from "react-native";
+import { Eye, EyeSlash } from "phosphor-react-native";
+import { useState } from "react";
+import { RFPercentage } from "react-native-responsive-fontsize";
+import { TextInput } from "../../components";
+import theme from "../../theme";
+import {
+  ForgotPasswordText,
+  Gradient,
+  HighlightedSignUpText,
+  LoginBackground,
+  LoginButton,
+  LoginButtonText,
+  LoginFormContainer,
+  LoginFormTitle,
+  Logo,
+  LogoContainer,
+  LogoGoogle,
+  LogoText,
+  SignUpText,
+  SocialLoginButton,
+  SocialLoginButtonLabel,
+  SocialLoginTitle,
+  SocialLoginTitleContainer,
+} from "./style";
 
 export const Login = () => {
-  return <Text>Login</Text>;
+  const ICON_SIZE = RFPercentage(3.3);
+  const ICON_COLOR = theme.colors.primary_500;
+  const [isPasswordHidden, setPasswordHidden] = useState(true);
+
+  return (
+    <LoginBackground>
+      <LogoContainer>
+        <Logo />
+        <LogoText>Para entregadores</LogoText>
+      </LogoContainer>
+
+      <LoginFormContainer>
+        <LoginFormTitle>Login</LoginFormTitle>
+        <TextInput
+          title="Email ou Telefone"
+          placeholder="example@pigz.com.br"
+        />
+
+        <TextInput
+          title="Senha"
+          placeholder="********"
+          hidePassword={isPasswordHidden}
+          icon={
+            isPasswordHidden ? (
+              <EyeSlash size={ICON_SIZE} color={ICON_COLOR} />
+            ) : (
+              <Eye size={ICON_SIZE} color={ICON_COLOR} />
+            )
+          }
+          onIconPress={() => setPasswordHidden((prev) => !prev)}
+        />
+      </LoginFormContainer>
+
+      <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+
+      <LoginButton>
+        <Gradient>
+          <LoginButtonText>Entrar</LoginButtonText>
+        </Gradient>
+      </LoginButton>
+
+      <SignUpText>
+        Não tem uma conta?{" "}
+        <HighlightedSignUpText>Criar agora!</HighlightedSignUpText>{" "}
+      </SignUpText>
+
+      <SocialLoginTitleContainer>
+        <SocialLoginTitle>Entrar com</SocialLoginTitle>
+      </SocialLoginTitleContainer>
+
+      <SocialLoginButton>
+        <LogoGoogle />
+        <SocialLoginButtonLabel>Continuar com o Google</SocialLoginButtonLabel>
+      </SocialLoginButton>
+    </LoginBackground>
+  );
 };
---
+++ src/screens/Login/style.ts
@@ -0,0 +1,117 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+import LogoSvg from "../../assets/svgs/logo.svg";
+import { Background, Text } from "../../components/styled";
+import theme from "../../theme";
+
+import Google from "../../assets/svgs/logoGoogle.svg";
+
+export const LoginBackground = styled(Background)`
+  padding: 0 ${RFPercentage(3.1)}px;
+`;
+
+export const Logo = styled(LogoSvg).attrs({
+  width: RFPercentage(12.8),
+  /* design do figma usado com tela 414x896, uso da lib react-native-responsive-fontsize para converter as proporcoes */
+  height: RFPercentage(6.4),
+})``;
+
+export const LogoText = styled(Text).attrs({
+  weight: "semibold",
+})`
+  font-size: ${RFValue(16)}px;
+  /* convertendo o RFValue fica 18px no Android Pixel 4 (celular usado como base) */
+  color: ${({ theme }) => theme.colors.gray_800};
+`;
+
+export const LoginFormTitle = styled(LogoText)`
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.gray_700};
+`;
+
+export const LoginInputTitle = styled(Text)`
+  font-size: ${RFValue(10)}px;
+`;
+
+export const LogoContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+  gap: ${RFPercentage(0.8)}px;
+  margin-top: ${RFPercentage(12)}px;
+  margin-bottom: ${RFPercentage(4.7)}px;
+`;
+
+export const LoginFormContainer = styled.View`
+  gap: ${RFPercentage(2.2)}px;
+`;
+
+export const ForgotPasswordText = styled(Text)`
+  font-size: ${RFValue(11.5)}px;
+  color: ${({ theme }) => theme.colors.gray_100};
+  text-decoration: underline;
+  margin-top: ${RFPercentage(3.1)}px;
+`;
+
+export const LoginButton = styled.TouchableOpacity`
+  margin-top: ${RFPercentage(3.1)}px;
+`;
+
+export const LoginButtonText = styled(Text).attrs({
+  weight: "medium",
+})`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${RFValue(14)}px;
+`;
+
+export const Gradient = styled(LinearGradient).attrs({
+  colors: [theme.colors.primary_200, theme.colors.primary_500],
+  start: { x: 0.1, y: 0 },
+})`
+  align-items: center;
+  justify-content: center;
+  height: ${RFPercentage(6.1)}px;
+  border-radius: 16px;
+`;
+
+export const SignUpText = styled(ForgotPasswordText)`
+  text-decoration: none;
+  align-self: center;
+`;
+
+export const HighlightedSignUpText = styled(SignUpText)`
+  color: ${({ theme }) => theme.colors.primary_500};
+`;
+
+export const SocialLoginTitle = styled(LogoText)`
+  font-size: ${RFValue(11.7)}px;
+  position: absolute;
+  background-color: ${({ theme }) => theme.colors.white};
+  padding-right: 5px;
+`;
+
+export const SocialLoginTitleContainer = styled.View`
+  border-bottom-width: 1px;
+  justify-content: center;
+  margin-top: ${RFPercentage(9)}px;
+`;
+
+export const SocialLoginButton = styled.TouchableOpacity`
+  border: 1px solid ${({ theme }) => theme.colors.gray_200};
+  border-radius: 16px;
+  height: ${RFPercentage(6.2)}px;
+  align-items: center;
+  justify-content: center;
+  margin-top: ${RFPercentage(3.1)}px;
+  flex-direction: row;
+`;
+
+export const SocialLoginButtonLabel = styled(LoginButtonText)`
+  color: ${({ theme }) => theme.colors.gray_100};
+  top: ${RFPercentage(0.25)}px;
+`;
+
+export const LogoGoogle = styled(Google)`
+  position: absolute;
+  left: 5%;
+`;
